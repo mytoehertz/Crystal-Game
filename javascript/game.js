895358754c59b16c68cb60767cf16186326bc17e
mytoehertz <juanitosemail@gmail.com>
2019-02-12 19:06:44 -0500

    Fixed the crys1, crys2, crys3, crys4

--- javascript/game.js
+++ javascript/game.js
@@ -10,12 +10,13 @@ let crys2 = Math.floor(Math.random()* 30) + 1;
 let crys3 = Math.floor(Math.random()* 40) + 15;
 let crys4 = Math.floor(Math.random()* 10) + 3;
 
+startgame();
 //Functions
 function winner () {
     if (totalScore === initialNumber) {
         win++;
-        reset();
         $("#wins").html("You have " + win + " wins");
+        reset();
     } else if (totalScore > initialNumber) {
         lose++;
         $("#loses").html("You have " + lose + " loses");
@@ -23,12 +24,12 @@ function winner () {
     }
 }
 function startgame() {
-
+    let initialNumber = Math.floor(Math.random() * 80) + 20;
 };
 
 function reset() {
 totalScore = 0;
-initialNumber = Math.Floor(Math.random() * 80) + 20;
+let initialNumber = Math.Floor(Math.random() * 80) + 20;
 // $("#Ore-Cart").html("You can carry up to " + initialNumber + " lbs.");
 }
 
@@ -36,23 +37,27 @@ initialNumber = Math.Floor(Math.random() * 80) + 20;
 $("#Ore-Cart").html("You can carry up to " + initialNumber + " lbs.");
 
 $("#crys1").click(function(){
+
     totalScore += crys1;
     $('#total-score').html(totalScore);
     winner();
     });
 
 $("#crys2").click(function(){
+    
     totalScore += crys2;
     $("#total-score").html(totalScore);
     winner();
     });
 
 $("#crys3").click(function(){
+    
     totalScore += crys3;
     $("#total-score").html(totalScore);
     winner();
         });
 $("#crys4").click(function(){
+    
     totalScore += crys4;
     $("#total-score").html(totalScore);
     winner();
